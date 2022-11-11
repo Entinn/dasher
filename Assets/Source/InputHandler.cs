d52d6fc5d9444d8a7c4e8222cd4a79da238d9b00
DESKTOP-J7VRBG4\Entin <K-anton-97@mail.ru>
2022-11-11 16:06:02 -0300

    remove active setting from inputhandler

--- Assets/Source/InputHandler.cs
+++ Assets/Source/InputHandler.cs
@@ -4,11 +4,11 @@ namespace Dasher
 {
     internal class InputHandler : MonoBehaviour
     {
-        public float MouseAxisX => active ? Input.GetAxis("Mouse X") : 0;
-        public bool DashPressed => active && Input.GetKeyDown(KeyCode.LeftShift);
+        public float MouseAxisX => Input.GetAxis("Mouse X");
+        public bool DashPressed => Input.GetKeyDown(KeyCode.LeftShift);
 
-        public float HorizontalSmoothAxis => active ? horizontalSmoothAxis : 0;
-        public float VerticalSmoothAxis => active ? verticalSmoothAxis : 0;
+        public float HorizontalSmoothAxis => horizontalSmoothAxis;
+        public float VerticalSmoothAxis => verticalSmoothAxis;
 
         private float horizontalSmoothAxis;
         private float verticalSmoothAxis;
@@ -16,19 +16,12 @@ namespace Dasher
         private float velocityHorizontal, velocityVertical;
         private const float SmoothTime = .1f;
 
-        private bool active = true;
-
         private void Update()
         {
             var inputHorizontal = Input.GetAxis("Horizontal");
             var inputVertical = Input.GetAxis("Vertical");
             horizontalSmoothAxis = Mathf.SmoothDamp(horizontalSmoothAxis, inputHorizontal, ref velocityHorizontal, SmoothTime);
             verticalSmoothAxis = Mathf.SmoothDamp(verticalSmoothAxis, inputVertical, ref velocityVertical, SmoothTime);
-        }
-
-        public void SetActive(bool active)
-        {
-            this.active = active;
         }
     }
 }
