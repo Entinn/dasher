cbb9bc00ac86c81d07d0ba47ae6ec8ab9ef5a96f
DESKTOP-J7VRBG4\Entin <K-anton-97@mail.ru>
2022-11-13 17:32:48 -0300

    camera vertical movement

---
+++ Assets/Source/CameraMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Dasher
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        private const float YMin = 10;
+        private const float YMax = 60;
+
+        [SerializeField]
+        private float distance = 10;
+
+        [SerializeField]
+        private float sensitivity = 150;
+
+        private Transform target;
+
+        private float currentX;
+        private float currentY;
+
+        private void LateUpdate()
+        {
+            if (!target)
+                return;
+
+            currentX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            currentY -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+            currentY = Mathf.Clamp(currentY, YMin, YMax);
+
+            Vector3 direction = new Vector3(0, 0, -distance);
+            Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+
+            var targetPosition = target.position;
+            transform.position = targetPosition + rotation * direction;
+
+            transform.LookAt(targetPosition);
+        }
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
